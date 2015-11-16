4f5d5ff6c0da19759233434559c7750f49c1b3c7
SON <SON@SONui-MacBook-Air.local>
2015-11-16 15:35:16 +0900

    Initial Commit

---
+++ NextNavigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NextNavigation
+//
+//  Created by SON on 2015. 11. 16..
+//  Copyright (c) 2015년 LearnToProgram.tv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
