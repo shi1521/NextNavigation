8036a1f59e19088e5a605b024915dfc7a4b656da
SON <SON@sonui-macbook-air.local>
2015-11-30 20:31:16 +0900

    로테이션 시 한글 추가 이미지 버튼 추가 테이블 뷰 추가

--- NextNavigation/ViewController.swift
+++ NextNavigation/ViewController.swift
@@ -16,17 +16,91 @@ class ViewController: UIViewController {
     @IBOutlet var firstBT: UIButton!
     
     override func viewDidLoad() {
+        print("viewDidLoad")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         firstBT.addTarget(self, action: "goFirstBT:", forControlEvents: UIControlEvents.TouchDown)
-        
         depth = self.navigationController?.viewControllers.count
-        self.navigationItem.title = "\(depth)"
-        currentLB.text = "\(depth)"
-        let longPress = UILongPressGestureRecognizer(target: self, action: Selector("handleLongPressGesture:"))
-        longPress.minimumPressDuration = 1.0
-        self.nextBT.gestureRecognizers = [longPress]
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "changeNumber", name: UIDeviceOrientationDidChangeNotification, object: nil)
+        changeNumber()
+        
+        //let longPress = UILongPressGestureRecognizer(target: self, action: Selector("handleLongPressGesture:"))
+        //longPress.minimumPressDuration = 1.0
+        //self.nextBT.gestureRecognizers = [longPress]
+    }
+    
+    func hangul(num: Int) -> String
+    {
+        let digit_1 : [String] = ["", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구"]
+        let digit_10 : [String] = ["십", "백", "천"]
+        let digit_10000 : [String] = ["만", "억", "조", "경"]
+        
+        var first : Int = 1
+        var pos_10 : Int = 0
+        var pos_10000 : Int = 0
+        var quotient : Int = num
+        var remainder : Int = 0
+        var hangul : String = ""
+        print(num)
+        
+        while(true)
+        {
+            if(quotient == 0)
+            {
+                break
+            }
+            remainder = quotient % 10
+            quotient = quotient / 10
+            //print(remainder)
+            //print(quotient)
+            if(first == 1)
+            {
+                first = 0
+                hangul = digit_1[remainder]
+                continue;
+            }
+        
+            if(pos_10 == 3)
+            {
+                pos_10 = 0
+                hangul = digit_1[remainder] + digit_10000[pos_10000++] + hangul
+            }
+            else
+            {
+                hangul = digit_1[remainder] + digit_10[pos_10++] + hangul
+            }
+        }
+        
+        print(hangul)
+        return hangul
+    }
+    
+    func changeNumber()
+    {
+        if(UIDeviceOrientationIsLandscape(UIDevice.currentDevice().orientation))
+        {
+            print("landscape")
+            //depth = self.navigationController?.viewControllers.count
+            self.navigationItem.title = hangul(depth)
+            currentLB.text = hangul(depth)
+            return
+        }
+        
+        if(UIDeviceOrientationIsPortrait(UIDevice.currentDevice().orientation))
+        {
+            print("Portrait")
+            self.navigationItem.title = "\(depth)"
+            currentLB.text = "\(depth)"
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "depth") {
+            let LongPressNavigationController = segue.destinationViewController as! UINavigationController
+            let LongPressViewController = LongPressNavigationController.viewControllers[0] as! LongPressView
+            LongPressViewController.depth = depth
+        }
     }
     
     func handleLongPressGesture(recogizer: UILongPressGestureRecognizer) {
@@ -41,6 +115,10 @@ class ViewController: UIViewController {
 
     @IBAction func goFirstBT(sender: AnyObject) {
         self.navigationController?.popToRootViewControllerAnimated(false)
+    }
+    
+    @IBAction func longPressed(sender: UILongPressGestureRecognizer) {
+        performSegueWithIdentifier("depth", sender: self)
     }
 }
 
--- NextNavigation/ViewLongPress.swift
+++ NextNavigation/ViewLongPress.swift
@@ -10,20 +10,23 @@ import UIKit
 
 class LongPressView: UITableViewController  {
     
-    var depth : Int!
+    var depth : Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        depth = self.navigationController?.viewControllers.count
     }
     
     // MARK: - TableViewDeagate
     override func tableView(tableview : UITableView, didSelectRowAtIndexPath indexPath : NSIndexPath){
         print("You selected row #\(indexPath.row)!")
-        self.navigationController?.viewControllers.removeRange((indexPath.row)...depth)
+        //self.navigationController?.viewControllers.removeRange((indexPath.row + 1)...depth)
+        let topNavigationController = self.presentingViewController as? UINavigationController
+        presentingViewController?.dismissViewControllerAnimated(true) { () -> Void in
+            assert(topNavigationController!.viewControllers.count > indexPath.row)
+            topNavigationController!.popToViewController(topNavigationController!.viewControllers[indexPath.row], animated: true)
+        }
     }
-    
     
     // MARK: - TableViewDataSource
     override func tableView(tableview: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,7 +35,7 @@ class LongPressView: UITableViewController  {
     
     override func tableView(tableview : UITableView, cellForRowAtIndexPath indexPath : NSIndexPath)->UITableViewCell{
         let cell : UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
-        cell.textLabel!.text = "\(indexPath.row)"
+        cell.textLabel!.text = "\(indexPath.row + 1)"
         return cell
     }
     
